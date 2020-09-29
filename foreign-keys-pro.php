4a29301995ebcb3f0758d1e28c8c1a285ec375a4
Brandon Hubbard <bhubbard@users.noreply.github.com>
2020-09-28 23:28:02 -0700

    Update foreign-keys-pro.php

--- foreign-keys-pro.php
+++ foreign-keys-pro.php
@@ -31,26 +31,42 @@ function fkp_myisam_check() {
 
 }
 
-
+add_action( 'admin_init', 'fkp_add_contstraints');
 function fkp_add_contstraints( $args = array() ) {
 
   global $wpdb;
 
+  $wpdb->show_errors();
+
+  // TOODO Arguements for on delete and on update
+
   // Default WordPress.
-  $sql = "ALTER TABLE $wpbd->usermeta ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `wp_users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $sql = "ALTER TABLE $wpbd->usermeta ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql);
 
-  $sql2 = "ALTER TABLE $wpdb->postmeta ADD CONSTRAINT `fk_post_id` FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $sql2 = "ALTER TABLE $wpdb->postmeta ADD CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql2);
 
-  $sql3 = "ALTER TABLE `wp_commentmeta` ADD CONSTRAINT `fk_comment_id` FOREIGN KEY (`comment_id`) REFERENCES `wp_comments` (`comment_ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $sql3 = "ALTER TABLE $wpdb->commentmeta ADD CONSTRAINT `comment_id` FOREIGN KEY (`comment_id`) REFERENCES $wpdb->comments (`comment_ID`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql3);
 
-  $sql4 = "ALTER TABLE `wp_termmeta` ADD CONSTRAINT `fk_term_id` FOREIGN KEY (`term_id`) REFERENCES `wp_terms` (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $sql4 = "ALTER TABLE $wpdb->termmeta ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql4);
 
-  // WooCommerce.
+  $sql5 = "ALTER TABLE $wpdb->term_relationships ADD CONSTRAINT `term_taxonomy_id` FOREIGN KEY (`term_taxonomy_id`) REFERENCES $wpdb->term_taxonomy (`term_taxonomy_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $wpdb->query($sql5);
 
+  $sql6 = "ALTER TABLE $wpdb->term_taxonomy ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $wpdb->query($sql6);
+
+  $sql7 = "ALTER TABLE $wpdb->posts ADD CONSTRAINT `author_id` FOREIGN KEY (`post_author`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+  $wpdb->query($sql7);
+
+  $sql8 = "ALTER TABLE $wpdb->comments ADD CONSTRAINT `commet_post_id` FOREIGN KEY (`comment_post_ID`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;";
+  $wpdb->query($sql8);
+
+  // WooCommerce.
+  /*
   $sql5 = "ALTER TABLE `wp_woocommerce_order_itemmeta` ADD CONSTRAINT `fk_order_item_id` FOREIGN KEY (`order_item_id`) REFERENCES `wp_woocommerce_order_items` (`order_item_id`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql5);
 
@@ -77,5 +93,5 @@ function fkp_add_contstraints( $args = array() ) {
 
   $sql12 = "ALTER TABLE `wp_og6o50l3ph_gf_addon_feed` ADD CONSTRAINT `fk_form_id` FOREIGN KEY (`form_id`) REFERENCES `wp_og6o50l3ph_gf_form` (`id`) ON DELETE CASCADE ON UPDATE CASCADE";
   $wpdb->query($sql12);
-
+  */
 }
