9a44be6ee6253d03a3104d554d01a1d8b4b08025
Brandon Hubbard <bkhubbard@gmail.com>
2020-09-29 10:19:11 -0700

    class cleanup

--- foreign-keys-pro.php
+++ foreign-keys-pro.php
@@ -19,54 +19,128 @@
  */
 
 
+if( ! class_exists( 'ForeignKeysPro' ) ) {
 
+	/**
+	 * Foreign Keys Pro.
+	 */
+	class ForeignKeysPro {
 
+		/**
+		 * [__construct description]
+		 */
+		public function __construct() {
 
-function fkp_myisam_check() {
-  global $wpdb;
-  $myisam_tables = intval( $wpdb->query(  "SHOW TABLE STATUS WHERE Engine = 'MyISAM'") );
+		}
 
-  if( 0 === $myisam_tables ) {
-    return false;
-  } else {
-    return true;
-  }
+		/**
+		 * [check_for_myisam description]
+		 * @return [type] [description]
+		 */
+		public function check_for_myisam() {
 
-}
+			global $wpdb;
+			$myisam_tables = intval( $wpdb->query(  "SHOW TABLE STATUS WHERE Engine = 'MyISAM'") );
 
-add_action( 'admin_init', 'fkp_add_contstraints');
-function fkp_add_contstraints( $args = array() ) {
+			if( 0 === $myisam_tables ) {
+				return false;
+			} else {
+				return true;
+			}
 
-  global $wpdb;
+		}
 
-  $wpdb->show_errors();
+		/**
+		 * [foreign_key_usermeta description]
+		 * @param  array  $args [description]
+		 * @return [type]       [description]
+		 */
+		public function foreign_key_usermeta( $args = array() ) {
 
-  // TOODO Arguements for on delete and on update
+			global $wpdb;
 
-  // Default WordPress.
-  $sql = "ALTER TABLE $wpbd->usermeta ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql);
+			$query = "ALTER TABLE $wpbd->usermeta ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+		  $results = $wpdb->query( $query ) ?? false;
 
-  $sql2 = "ALTER TABLE $wpdb->postmeta ADD CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql2);
+			return $results;
 
-  $sql3 = "ALTER TABLE $wpdb->commentmeta ADD CONSTRAINT `comment_id` FOREIGN KEY (`comment_id`) REFERENCES $wpdb->comments (`comment_ID`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql3);
+		}
 
-  $sql4 = "ALTER TABLE $wpdb->termmeta ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql4);
+		public function foreign_key_postmeta( $args = array() ) {
 
-  $sql5 = "ALTER TABLE $wpdb->term_relationships ADD CONSTRAINT `term_taxonomy_id` FOREIGN KEY (`term_taxonomy_id`) REFERENCES $wpdb->term_taxonomy (`term_taxonomy_id`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql5);
+			global $wpdb;
 
-  $sql6 = "ALTER TABLE $wpdb->term_taxonomy ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql6);
+			$query = "ALTER TABLE $wpdb->postmeta ADD CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
 
-  $sql7 = "ALTER TABLE $wpdb->posts ADD CONSTRAINT `author_id` FOREIGN KEY (`post_author`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
-  $wpdb->query($sql7);
+			return $results;
 
-  $sql8 = "ALTER TABLE $wpdb->comments ADD CONSTRAINT `commet_post_id` FOREIGN KEY (`comment_post_ID`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;";
-  $wpdb->query($sql8);
+		}
 
+		public function foreign_key_commentmeta( $args = array() ) {
+
+			global $wpdb;
+
+			$query = "ALTER TABLE $wpdb->commentmeta ADD CONSTRAINT `comment_id` FOREIGN KEY (`comment_id`) REFERENCES $wpdb->comments (`comment_ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
+
+			return $results;
+
+		}
+
+		public function foregin_key_termmeta( $args = array() ) {
+
+			global $wpdb;
+
+			$query = "ALTER TABLE $wpdb->termmeta ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
+
+			return $results;
+
+		}
+
+		public function foregin_key_term_relationships( $args = array() ) {
+
+			global $wpdb;
+
+			$query = "ALTER TABLE $wpdb->term_relationships ADD CONSTRAINT `term_taxonomy_id` FOREIGN KEY (`term_taxonomy_id`) REFERENCES $wpdb->term_taxonomy (`term_taxonomy_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
+
+			return $results;
+
+		}
+
+		public function foregin_key_term_taxonomy( $args = array() ) {
+
+			global $wpdb;
+
+			$query = "ALTER TABLE $wpdb->term_taxonomy ADD CONSTRAINT `term_id` FOREIGN KEY (`term_id`) REFERENCES $wpdb->terms (`term_id`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
+
+			return $results;
+
+		}
+
+		public function foregin_key_posts( $args = array() ) {
+
+			global $wpdb;
+
+			$query = "ALTER TABLE $wpdb->posts ADD CONSTRAINT `author_id` FOREIGN KEY (`post_author`) REFERENCES $wpdb->users (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+			$results = $wpdb->query( $query ) ?? false;
+
+			return $results;
+
+		}
+
+		public function foreign_keys_comments( $args = array() ) {
+
+				global $wpdb;
+
+				$query = "ALTER TABLE $wpdb->comments ADD CONSTRAINT `commet_post_id` FOREIGN KEY (`comment_post_ID`) REFERENCES $wpdb->posts (`ID`) ON DELETE CASCADE ON UPDATE CASCADE";
+				$results = $wpdb->query( $query ) ?? false;
+
+				return $results;
+
+		}
 
 }
